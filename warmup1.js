2a7f5cd61d6801b96fc5d36652508707ac1a49c6
Josh <empjoshkc@gmail.com>
2022-02-21 17:35:21 +0000

    second commits

--- warmup1.js
+++ warmup1.js
@@ -1,1 +1,9 @@
-console.log('cheese')
+// let myStr = "Cheartse".indexOf("s1")
+// console.log(myStr);
+
+// Function to find string locations in a text irrespective of case
+function indexOfIgnoreCase(str1, str2) {
+    let index = str1.toLowerCase().indexOf(str2.toLowerCase())
+    return index
+}
+console.log(indexOfIgnoreCase('astronomical COnjunction', 'J'));
