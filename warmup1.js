cb52e06e419f2fd1219c55aafb8b0ad7413631a2
Josh <empjoshkc@gmail.com>
2022-02-22 23:16:42 +0000

    warmup2.js

--- warmup1.js
+++ warmup1.js
@@ -2,8 +2,10 @@
 // console.log(myStr);
 
 // Function to find string locations in a text irrespective of case
+// 
 function indexOfIgnoreCase(str1, str2) {
     let index = str1.toLowerCase().indexOf(str2.toLowerCase())
     return index
 }
 console.log(indexOfIgnoreCase('astronomical COnjunction', 'J'));
+
